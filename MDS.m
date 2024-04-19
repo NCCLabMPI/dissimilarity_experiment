7986613e50dc979f7496ad1d26c7ee5b300cbaf0
EceZiya <144265169+EceZiya@users.noreply.github.com>
2024-04-19 17:31:41 +0200

    MDS ready
    this script can plot MDS in 2D scale

--- MDS.m
+++ MDS.m
@@ -10,7 +10,10 @@ config;
 cd(processedDataPath)
 addpath(genpath(processedDataPath));
 
-load("FaceMeanRatingsTable.mat")
+load("FaceMeanRatingsTable.mat");
+load("ObjectMeanRatingsTable.mat");
+
+
 
 % aggregate ratings into one rating
 
@@ -103,7 +106,6 @@ end
         
         % plot the MDS 
 
-        
 figure;
         stimulusNames = {'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'};
         xlabel('Distance')
