fd44766a546f190dd9ef8bd6726ef0a294ae326c
Alex Lepauvre <alexander.lepauvre@ad.aesthetics.mpg.de>
2024-04-21 14:15:23 +0200

    Save intrarater reliability to table

--- intraSubReliabilty.m
+++ intraSubReliabilty.m
@@ -10,13 +10,17 @@ cd(processedDataPath);
 load('FaceData.mat');
 disp('Face')
 face_correlationData = combinedFaceCells; % change here to do the same test for objects
-intra_rater_reliability(face_correlationData)
+faceIntraReliability = intra_rater_reliability(face_correlationData);
 
 load('ObjectData.mat');
 disp(' ')
 disp('Object')
 object_correlationData = combinedObjectCells; % change here to do the same test for objects
-intra_rater_reliability(object_correlationData)
+objectIntraReliability = intra_rater_reliability(object_correlationData);
+
+% Save the tables:
+save(fullfile(processedDataPath,"faceIntraRaterReliability.mat"),'faceIntraReliability');
+save(fullfile(processedDataPath,"objectIntraRaterReliability.mat"),'objectIntraReliability');
 
 
 
